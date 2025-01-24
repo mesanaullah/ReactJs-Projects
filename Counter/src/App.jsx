2c2ea835fe4672b59ec5454ab73baf72ef3a9b5c
mesanaullah <mdsanaullah1227@gmail.com>
2025-01-24 18:12:49 +0530

    New Add

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -37,7 +37,8 @@ function App() {
       <div className="">
         <h1 className='p-10 text-green-400'>{Count}</h1>
         <div className="btn">
-          <button className="bg-green-400 rounded-xl" onClick={Add} >Add</button>
+          {/* <button className="bg-green-400 rounded-xl" onClick={Add} >Add</button> */}
+          <button className="bg-green-400 rounded-xl" onClick={() => setCount(Count + 1)} >Add</button>
           <button className="bg-green-400 rounded-xl" onClick={Delete} >Delete</button>
         </div>
         <button className="bg-green-400 rounded-xl" onClick={Reset}>Reset</button>
