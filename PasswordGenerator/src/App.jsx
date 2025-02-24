63e81c42fd920b2bce937494017d0dcf6844ad34
mesanaullah <mdsanaullah1227@gmail.com>
2025-02-24 17:39:56 +0530

    Password Generator

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   const [length, setLength] = useState(8)
   const [numAllowed, setNumAllowed] = useState(false)
-  const [charAllowed, seCharAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
 
   //useRef hook
@@ -17,7 +17,7 @@ function App() {
     if (numAllowed) str += "0123456789"
     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    for (let i = 1; i < length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
       pass += str.charAt(char)
     }
@@ -26,19 +26,19 @@ function App() {
 
   }, [length, numAllowed, charAllowed, setPassword])
 
-  useEffect(() => {
-    PasswordGenerator()
-  }, [length, numAllowed, charAllowed, PasswordGenerator])
-
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
     passwordRef.current?.setSelectionRange(0, 999);
     window.navigator.clipboard.writeText(password)
   }, [password])
 
+  useEffect(() => {
+    PasswordGenerator()
+  }, [length, numAllowed, charAllowed, PasswordGenerator])
+
   return (
     <div className="w-full max-w-1/2 h-full min-h-full mx-auto shadow-md rounded-lg px-4 py-3 my-10 bg-gray-800 text-orange-500">
-      <h1 className=' text-xl text-white font-semibold text-center my-3'>Password generator</h1>
+      <h1 className=' text-xl text-white font-semibold text-center my-3'>Password Generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4 ">
         <input
           type="text"
@@ -91,7 +91,7 @@ function App() {
             defaultChecked={charAllowed}
             id="characterInput"
             onChange={() => {
-              setNumberAllowed((prev) => !prev);
+              setCharAllowed((prev) => !prev)
             }}
           />
           <label htmlFor="characterInput">Characters</label>
